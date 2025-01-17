5a9ed5c1394e8a10ee7a2153222b999a60f55456
qxb3 <qxbthree@gmail.com>
2025-01-18 02:46:28 +0800

    more actions (#17)
    * shuffle_off(), shuffle_toggle(), shuffle_on() actions
    
    * loop_none(), loop_track(), loop_playlist()
    
    * stop(), play(), pause()

--- src/fum.rs
+++ src/fum.rs
@@ -1,7 +1,7 @@
 use std::{io::{stdout, Stdout}, process::Command, time::Duration};
 
 use crossterm::{event::{EnableMouseCapture, Event, KeyCode, KeyEventKind, MouseButton, MouseEventKind}, execute};
-use mpris::Player;
+use mpris::{LoopStatus, Player};
 use ratatui::{layout::Position, prelude::CrosstermBackend, Terminal};
 use ratatui_image::picker::Picker;
 
@@ -86,9 +86,21 @@ impl<'a> Fum<'a> {
                             // Execute action
                             if let Some(action) = action {
                                 match action.as_str() {
+                                    "stop()" => self.stop(),
+                                    "play()" => self.play(),
+                                    "pause()" => self.pause(),
+
                                     "prev()" => self.prev(),
                                     "play_pause()" => self.play_pause(),
                                     "next()" => self.next(),
+
+                                    "shuffle_off()" => self.shuffle("off"),
+                                    "shuffle_toggle()" => self.shuffle("toggle"),
+                                    "shuffle_on()" => self.shuffle("on"),
+
+                                    "loop_none()" => self.r#loop("none"),
+                                    "loop_track()" => self.r#loop("track"),
+                                    "loop_playlist()" => self.r#loop("playlist"),
                                     _ => {}
                                 }
                             }
@@ -130,6 +142,24 @@ impl<'a> Fum<'a> {
         self.meta = Meta::default();
     }
 
+    fn stop(&self) {
+        if let Some(player) = &self.player {
+            player.stop().expect("Failed to stop player");
+        }
+    }
+
+    fn play(&self) {
+        if let Some(player) = &self.player {
+            player.play().expect("Failed to play player");
+        }
+    }
+
+    fn pause(&self) {
+        if let Some(player) = &self.player {
+            player.pause().expect("Failed to pause player");
+        }
+    }
+
     fn prev(&self) {
         if let Some(player) = &self.player {
             player.previous().expect("Failed to prev player");
@@ -145,6 +175,32 @@ impl<'a> Fum<'a> {
     fn next(&self) {
         if let Some(player) = &self.player {
             player.next().expect("Failed to next player");
+        }
+    }
+
+    fn shuffle(&self, shuffle: &str) {
+        if let Some(player) = &self.player {
+            if player.can_shuffle().expect("Failed to get if player can shuffle") {
+                match shuffle {
+                    "off" => player.set_shuffle(false).expect("Failed to set off shuffle"),
+                    "toggle" => player.set_shuffle(!player.get_shuffle().expect("Failed to get shuffle state")).expect("Failed to toggle shuffle"),
+                    "on" => player.set_shuffle(true).expect("Failed to set on shuffle"),
+                    _ => unreachable!()
+                }
+            }
+        }
+    }
+
+    fn r#loop(&self, r#loop: &str) {
+        if let Some(player) = &self.player {
+            if player.can_loop().expect("Failed to get if player can loop") {
+                match r#loop {
+                    "none" => player.set_loop_status(LoopStatus::None).expect("Failed to set loop to none"),
+                    "track" => player.set_loop_status(LoopStatus::Track).expect("Failed to set loop to track"),
+                    "playlist" => player.set_loop_status(LoopStatus::Playlist).expect("Failed to set loop to playlist"),
+                    _ => unreachable!()
+                }
+            }
         }
     }
 }
