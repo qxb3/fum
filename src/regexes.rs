a12f3e1efd4913fc3155190570e2cbd6bdd9a64d
qxbthree <qxbthree@gmail.com>
2025-02-02 14:35:56 +0800

    support for hyphen on var

--- src/regexes.rs
+++ src/regexes.rs
@@ -8,5 +8,5 @@ lazy_static! {
     pub static ref VAR_SET_RE: Regex = Regex::new(r"set\((\$\w[-\w]*),\s*(\$\w[-\w]*)\)").unwrap();
 
     pub static ref GET_META_RE: Regex = Regex::new(r"get_meta\((.*?)\)").unwrap();
-    pub static ref VAR_RE: Regex = Regex::new(r"var\((\$\w+),\s*(\$\w+)\)").unwrap();
+    pub static ref VAR_RE: Regex = Regex::new(r"var\((\$\w[\w-]*),\s*(\$\w[\w-]*)\)").unwrap();
 }
