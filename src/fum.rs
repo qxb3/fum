2f23faa2f8340889411246a51ac553d20fc5ff37
qxb3 <qxbthree@gmail.com>
2025-02-17 10:51:11 +0800

    added mouse keybinds & keybind modifier (#91) (#92)
    * action-secondary
    
    * keybind modifier
    
    * fix keybind modifier

--- src/fum.rs
+++ src/fum.rs
@@ -89,6 +89,21 @@ impl<'a> Fum<'a> {
                         match keybind {
                             Keybind::Many(keybinds) => {
                                 for keybind in keybinds {
+                                    if let Keybind::WithModifier(modifiers, keybind) = keybind {
+                                        if key.modifiers.intersects(*modifiers) {
+                                            if key.code == keybind.into_keycode() {
+                                                Action::run(action, self)?;
+                                            }
+                                        }
+                                    } else {
+                                        if key.code == keybind.into_keycode() {
+                                            Action::run(action, self)?;
+                                        }
+                                    }
+                                }
+                            },
+                            Keybind::WithModifier(modifiers, keybind) => {
+                                if key.modifiers.intersects(*modifiers) {
                                     if key.code == keybind.into_keycode() {
                                         Action::run(action, self)?;
                                     }
@@ -102,70 +117,92 @@ impl<'a> Fum<'a> {
                         }
                     }
                 },
-                Event::Mouse(mouse) if mouse.kind == MouseEventKind::Down(MouseButton::Left) => {
-                    if let Some((action, exec)) = self.ui.click(mouse.column, mouse.row, &self.state.buttons) {
-                        let action = action.to_owned();
-                        let exec = exec.to_owned();
+                Event::Mouse(mouse) => {
+                    match mouse.kind {
+                        // Button click mouse left.
+                        MouseEventKind::Down(MouseButton::Left) => {
+                            if let Some((action, _, exec)) = self.ui.click(mouse.column, mouse.row, &self.state.buttons) {
+                                let action = action.to_owned();
+                                let exec = exec.to_owned();
 
-                        if let Some(action) = action {
-                            Action::run(&action, self)?;
-                        }
+                                if let Some(action) = action {
+                                    Action::run(&action, self)?;
+                                }
 
-                        if let Some(exec) = exec {
-                            let parts: Vec<&str> = exec.split_whitespace().collect();
-                            if let Some(command) = parts.get(0) {
-                                let _ = Command::new(command) // Ignore result
-                                    .args(&parts[1..])
-                                    .stdout(Stdio::null())
-                                    .stderr(Stdio::null())
-                                    .spawn();
+                                if let Some(exec) = exec {
+                                    let parts: Vec<&str> = exec.split_whitespace().collect();
+                                    if let Some(command) = parts.get(0) {
+                                        let _ = Command::new(command) // Ignore result
+                                            .args(&parts[1..])
+                                            .stdout(Stdio::null())
+                                            .stderr(Stdio::null())
+                                            .spawn();
+                                    }
+                                }
                             }
-                        }
-                    }
-                },
-                Event::Mouse(mouse) if mouse.kind == MouseEventKind::Drag(MouseButton::Left) => {
-                    if !self.dragging && self.start_drag.is_none() {
-                        self.dragging = true;
-                        self.start_drag = Some(Position::new(mouse.column, mouse.row));
-                    }
+                        },
 
-                    if self.dragging {
-                        self.current_drag = Some(Position::new(mouse.column, mouse.row));
+                        // Button click mouse right.
+                        MouseEventKind::Down(MouseButton::Right) => {
+                            if let Some((_, action_secondary, _)) = self.ui.click(mouse.column, mouse.row, &self.state.buttons) {
+                                let action_secondary = action_secondary.to_owned();
 
-                        if let Some(start_drag) = &self.start_drag {
-                            if let Some(current_drag) = &self.current_drag {
-                                if let Some((rect, direction, widget)) = self.ui.drag(start_drag, &self.state.sliders) {
-                                    let value: f64 = match direction {
-                                        Direction::Horizontal => ((current_drag.x as f64 - rect.x as f64) / rect.width as f64).clamp(0.0, 1.0),
-                                        Direction::Vertical => (1.0 - ((current_drag.y as f64 - rect.y as f64) / rect.height as f64)).clamp(0.0, 1.0)
-                                    };
+                                if let Some(action_secondary) = action_secondary {
+                                    Action::run(&action_secondary, self)?;
+                                }
+                            }
+                        },
 
-                                    match widget {
-                                        SliderSource::Progress => {
-                                            let position = value * self.state.meta.length.as_secs() as f64;
-                                            if position >= self.state.meta.length.as_secs() as f64 {
-                                                self.dragging = false;
-                                                self.start_drag = None;
-                                                self.current_drag = None;
+                        // Mouse left drag.
+                        MouseEventKind::Drag(MouseButton::Left) => {
+                            if !self.dragging && self.start_drag.is_none() {
+                                self.dragging = true;
+                                self.start_drag = Some(Position::new(mouse.column, mouse.row));
+                            }
+
+                            if self.dragging {
+                                self.current_drag = Some(Position::new(mouse.column, mouse.row));
+
+                                if let Some(start_drag) = &self.start_drag {
+                                    if let Some(current_drag) = &self.current_drag {
+                                        if let Some((rect, direction, widget)) = self.ui.drag(start_drag, &self.state.sliders) {
+                                            let value: f64 = match direction {
+                                                Direction::Horizontal => ((current_drag.x as f64 - rect.x as f64) / rect.width as f64).clamp(0.0, 1.0),
+                                                Direction::Vertical => (1.0 - ((current_drag.y as f64 - rect.y as f64) / rect.height as f64)).clamp(0.0, 1.0)
+                                            };
+
+                                            match widget {
+                                                SliderSource::Progress => {
+                                                    let position = value * self.state.meta.length.as_secs() as f64;
+                                                    if position >= self.state.meta.length.as_secs() as f64 {
+                                                        self.dragging = false;
+                                                        self.start_drag = None;
+                                                        self.current_drag = None;
+                                                    }
+
+                                                    Action::run(&Action::Position(position as u64), self)?;
+                                                },
+                                                SliderSource::Volume => {
+                                                    let volume = value * 100.0;
+                                                    Action::run(&Action::Volume(VolumeType::Set(volume)), self)?;
+                                                }
                                             }
-
-                                            Action::run(&Action::Position(position as u64), self)?;
-                                        },
-                                        SliderSource::Volume => {
-                                            let volume = value * 100.0;
-                                            Action::run(&Action::Volume(VolumeType::Set(volume)), self)?;
                                         }
                                     }
                                 }
                             }
-                        }
+                        },
+
+                        // Mouse left release.
+                        MouseEventKind::Up(MouseButton::Left) => {
+                            self.dragging = false;
+                            self.start_drag = None;
+                            self.current_drag = None;
+                        },
+
+                        _ => {}
                     }
                 },
-                Event::Mouse(mouse) if mouse.kind == MouseEventKind::Up(MouseButton::Left) => {
-                    self.dragging = false;
-                    self.start_drag = None;
-                    self.current_drag = None;
-                }
                 Event::Resize(_, _) => {
                     self.redraw = true;
                 }
--- src/state.rs
+++ src/state.rs
@@ -7,7 +7,7 @@ use crate::{action::Action, meta::Meta, widget::{Direction, SliderSource}};
 pub struct FumState {
     pub meta: Meta,
     pub cover_art_ascii: String,
-    pub buttons: HashMap<String, (Rect, Option<Action>, Option<String>)>,
+    pub buttons: HashMap<String, (Rect, Option<Action>, Option<Action>, Option<String>)>,
     pub sliders: HashMap<String, (Rect, Direction, SliderSource)>,
     pub vars: HashMap<String, String>,
     pub parent_direction: Direction,
