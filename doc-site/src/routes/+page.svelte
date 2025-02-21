7785cf6e13c26c71289e84c878a588aba5c0af64
qxbthree <qxbthree@gmail.com>
2025-02-21 11:30:24 +0800

    basic landing page

--- doc-site/src/routes/+page.svelte
+++ doc-site/src/routes/+page.svelte
@@ -1,2 +1,15 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<div class="h-screen max-w-[64rem] mx-auto">
+  <div class="h-screen flex flex-col justify-center items-center p-8 space-y-12 text-center">
+    <img
+      width="300px"
+      src="/logo.png"
+      alt="Logo"
+    />
+
+    <h1 class="text-4xl font-bold">fum: A fully ricable tui-based mpris music client. </h1>
+
+    <button class="border border-[#defedf] px-4 py-2 cursor-pointer">
+      Get Started
+    </button>
+  </div>
+</div>
