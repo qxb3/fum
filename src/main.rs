704d5f6edd3383e72def27fa3dde93ed9ffaed98
qxbthree <qxbthree@gmail.com>
2025-03-01 10:03:53 +0800

    mpris: more properties

--- src/main.rs
+++ src/main.rs
@@ -20,8 +20,22 @@ async fn main() -> FumResult<()> {
         let title = metadata.title()?.unwrap_or("No Title".into());
         let album = metadata.album()?.unwrap_or("No Album".into());
         let artists = metadata.artists()?.unwrap_or(vec!["No Artists".into()]);
-        let length = metadata.length()?.unwrap_or(Duration::from_secs_f32(99.0));
+        let length = metadata.length()?.unwrap_or(Duration::from_secs_f32(9999.0));
         let art_url = metadata.art_url()?.unwrap_or("No art url".into());
+
+        let playback_status = spotify.playback_status().await?;
+        let loop_status = spotify.loop_status().await?;
+        let playback_rate = spotify.playback_rate().await?;
+        let min_playback_rate = spotify.min_playback_rate().await?;
+        let max_playback_rate = spotify.max_playback_rate().await?;
+        let volume = spotify.volume().await?;
+        let position = spotify.position().await?;
+        let can_next = spotify.can_next().await?;
+        let can_prev = spotify.can_previous().await?;
+        let can_play = spotify.can_play().await?;
+        let can_pause = spotify.can_pause().await?;
+        let can_seek = spotify.can_seek().await?;
+        let can_control = spotify.can_control().await?;
 
         println!("trackid: {trackid}");
         println!("title: {title}");
@@ -29,6 +43,20 @@ async fn main() -> FumResult<()> {
         println!("artists: {:?}", artists);
         println!("length: {}", length.as_secs());
         println!("art url: {art_url}");
+
+        println!("playback status: {:?}", playback_status);
+        println!("loop status: {:?}", loop_status);
+        println!("playback rate: {playback_rate}");
+        println!("min playback rate: {min_playback_rate}");
+        println!("max playback rate: {max_playback_rate}");
+        println!("volume: {volume}");
+        println!("position: {}", position.as_secs());
+        println!("can next: {can_next}");
+        println!("can prev: {can_prev}");
+        println!("can play: {can_play}");
+        println!("can pause: {can_pause}");
+        println!("can seek: {can_seek}");
+        println!("can control: {can_control}");
     }
 
     Ok(())
--- src/mpris/player.rs
+++ src/mpris/player.rs
@@ -1,10 +1,26 @@
-use std::collections::HashMap;
+use std::{collections::HashMap, time::Duration};
 
 use zbus::{zvariant, Connection, Proxy};
 
 use crate::FumResult;
 
 use super::Metadata;
+
+/// Playback status of player.
+#[derive(Debug)]
+pub enum PlaybackStatus {
+    Playing,
+    Paused,
+    Stopped
+}
+
+/// Loop status of player.
+#[derive(Debug)]
+pub enum LoopStatus {
+    None,
+    Track,
+    Playlist
+}
 
 #[derive(Debug)]
 pub struct Player<'a> {
@@ -15,7 +31,7 @@ pub struct Player<'a> {
 }
 
 impl<'a> Player<'a> {
-    /// Creates a new player struct.
+    /// Creates a new Player.
     pub async fn new(connection: &'a Connection, bus_name: String) -> FumResult<Self> {
         // Proxy for "org.mpris.MediaPlayer2.Player" interface.
         let player_proxy = Proxy::new(
@@ -40,11 +56,114 @@ impl<'a> Player<'a> {
         })
     }
 
-    /// Get the player metadata.
+    /// Metadata of player.
     pub async fn metadata(&self) -> FumResult<Metadata> {
         let metadata: HashMap<String, zvariant::Value> =
             self.player_proxy.get_property("Metadata").await?;
 
         Ok(Metadata::new(metadata)?)
+    }
+
+    /// PlaybackStatus of player.
+    pub async fn playback_status(&self) -> FumResult<PlaybackStatus> {
+        let playback_status: String =
+            self.player_proxy.get_property("PlaybackStatus").await?;
+
+        match playback_status.as_str() {
+            "Playing" => Ok(PlaybackStatus::Playing),
+            "Paused" => Ok(PlaybackStatus::Paused),
+            "Stopped" => Ok(PlaybackStatus::Stopped),
+            _ => Err("PlaybackStatus is not Playing,Paused or Stopped.".into())
+        }
+    }
+
+    /// LoopStatus of player.
+    pub async fn loop_status(&self) -> FumResult<LoopStatus> {
+        let loop_status: String =
+            self.player_proxy.get_property("LoopStatus").await?;
+
+        match loop_status.as_str() {
+            "None" => Ok(LoopStatus::None),
+            "Track" => Ok(LoopStatus::Track),
+            "Playlist" => Ok(LoopStatus::Playlist),
+            _ => Err("PlaybackStatus is not None,Track or Playlist.".into())
+        }
+    }
+
+    /// Playback Rate of player.
+    pub async fn playback_rate(&self) -> FumResult<f64> {
+        let rate: f64 = self.player_proxy.get_property("Rate").await?;
+
+        Ok(rate)
+    }
+
+    /// Minimum Playback Rate of player.
+    pub async fn min_playback_rate(&self) -> FumResult<f64> {
+        let min_rate: f64 = self.player_proxy.get_property("MinimumRate").await?;
+
+        Ok(min_rate)
+    }
+
+    /// Maximum Playback Rate of player.
+    pub async fn max_playback_rate(&self) -> FumResult<f64> {
+        let max_rate: f64 = self.player_proxy.get_property("MaximumRate").await?;
+
+        Ok(max_rate)
+    }
+
+    /// Volume of player.
+    pub async fn volume(&self) -> FumResult<f64> {
+        let volume: f64 = self.player_proxy.get_property("Volume").await?;
+
+        Ok(volume)
+    }
+
+    /// Position of player.
+    pub async fn position(&self) -> FumResult<Duration> {
+        let position: i64 = self.player_proxy.get_property("Position").await?;
+
+        Ok(Duration::from_micros(position as u64))
+    }
+
+    /// Can the player go next.
+    pub async fn can_next(&self) -> FumResult<bool> {
+        let can_go_next: bool = self.player_proxy.get_property("CanGoNext").await?;
+
+        Ok(can_go_next)
+    }
+
+    /// Can the player go previous.
+    pub async fn can_previous(&self) -> FumResult<bool> {
+        let can_go_previous: bool = self.player_proxy.get_property("CanGoPrevious").await?;
+
+        Ok(can_go_previous)
+    }
+
+    /// Can the player play.
+    pub async fn can_play(&self) -> FumResult<bool> {
+        let can_play: bool = self.player_proxy.get_property("CanPlay").await?;
+
+        Ok(can_play)
+    }
+
+    /// Can the player pause.
+    pub async fn can_pause(&self) -> FumResult<bool> {
+        let can_pause: bool = self.player_proxy.get_property("CanPause").await?;
+
+        Ok(can_pause)
+    }
+
+    /// Can the player seek.
+    pub async fn can_seek(&self) -> FumResult<bool> {
+        let can_seek: bool = self.player_proxy.get_property("CanSeek").await?;
+
+        Ok(can_seek)
+    }
+
+    /// Can the player be controlled.
+    pub async fn can_control(&self) -> FumResult<bool> {
+        let can_control: bool = self.player_proxy.get_property("CanControl").await?;
+
+        Ok(can_control)
     }
 }
