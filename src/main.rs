761b004b742241c63f58ed9369cb14f46e7ba4a1
qxbthree <qxbthree@gmail.com>
2025-05-03 02:14:24 +0800

    remove testing stuff

--- src/main.rs
+++ src/main.rs
@@ -1,55 +1,30 @@
-// mod cli;
-// mod cover;
-// mod event;
-// mod fum;
-// mod mode;
+mod cli;
+mod cover;
+mod event;
+mod fum;
+mod mode;
 mod mpris;
-// mod player;
-// mod script;
-// mod state;
+mod player;
+mod script;
+mod state;
 mod status;
-// mod track;
-// mod ui;
-// mod utils;
-// mod widget;
+mod track;
+mod ui;
+mod utils;
+mod widget;
 
-use mpris::{Mpris, MprisEvent};
-
-// use fum::Fum;
+use fum::Fum;
 
 /// Type alias for global Result.
 type FumResult<T> = anyhow::Result<T>;
 
 #[tokio::main]
 async fn main() -> FumResult<()> {
-    let mut mpris = Mpris::new_without_options().await?;
-    let players = mpris.players();
-
-    mpris.watch();
-
-    while let Ok(event) = mpris.recv().await? {
-        match event {
-            MprisEvent::PlayerAttached(identity) => println!("ATTACHED = {:?}", identity),
-            MprisEvent::PlayerDetached(identity) => println!("DETACHED = {:?}", identity),
-            MprisEvent::PlayerPropertiesChanged(identity) => {
-                let players = players.try_lock()?;
-
-                if let Some(player) = players.iter().find(|p| *p.identity() == identity) {
-                    println!("PLAYER_PROP_CHANGED = {:#?}", player.metadata().await?)
-                }
-            }
-            MprisEvent::PlayerSeeked(identity) => println!("PLAYER_SEEKED = {:?}", identity),
-            MprisEvent::PlayerPosition(identity, position) => {
-                println!("PLAYER_POSITION = {:?} {:?}", identity, position.as_secs())
-            }
-        }
+    // None variant signifies that we shouldn't start fum tui.
+    if let Some(args) = cli::run().await? {
+        let mut fum = Fum::new(&args).await?;
+        fum.start(args.mode).await?;
     }
-
-    // // None variant signifies that we shouldn't start fum tui.
-    // if let Some(args) = cli::run().await? {
-    //     let mut fum = Fum::new(&args).await?;
-    //     fum.start(args.mode).await?;
-    // }
 
     Ok(())
 }
