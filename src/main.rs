533d4cf4246353e6dc4369ae76097d2c7dbc2bdb
qxbthree <qxbthree@gmail.com>
2025-05-03 04:47:14 +0800

    remove all stuff except mpris because mpris is awesome and i like it

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,12 @@
-mod cli;
-mod cover;
-mod event;
-mod fum;
-mod mode;
 mod mpris;
-mod player;
-mod script;
-mod state;
 mod status;
-mod track;
-mod ui;
-mod utils;
-mod widget;
-
-use fum::Fum;
 
 /// Type alias for global Result.
 type FumResult<T> = anyhow::Result<T>;
 
 #[tokio::main]
 async fn main() -> FumResult<()> {
-    // None variant signifies that we shouldn't start fum tui.
-    if let Some(args) = cli::run().await? {
-        let mut fum = Fum::new(&args).await?;
-        fum.start(args.mode).await?;
-    }
+    println!("HAHAHAHAHAHAHAHAHAHAH");
 
     Ok(())
 }
--- src/utils/mod.rs
+++
@@ -1,3 +0,0 @@
-pub mod duration;
-pub mod interaction;
-pub mod text;
